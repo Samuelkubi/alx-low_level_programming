d5729384e4422e2482a8c46b9757d6471f4d13d6
Samuelkubi <samuelkubi231@gmail.com>
2023-02-21 19:24:32 -0800

    task 0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+void print _alphabet(void);
+
+void print _alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print _sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_table(void);
+
+void times_tabel(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+#endif
